02c1788b37d5bdb0bc88bd85d407fd1e6307b248
Djordje Knezevic <starwarrior@mycity.rs>
2023-02-22 18:45:17 +0100

    Initial Commit

---
+++ I Am Rich SwiftUI/I_Am_Rich_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_Am_Rich_SwiftUIApp.swift
+//  I Am Rich SwiftUI
+//
+//  Created by Djordje Knezevic on 22.2.23..
+//
+
+import SwiftUI
+
+@main
+struct I_Am_Rich_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
