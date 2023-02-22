6efb380a6d53bdafca79b686b61a10b761ec11a9
Djordje Knezevic <starwarrior@mycity.rs>
2023-02-22 19:11:36 +0100

    Change text, font size, weight, font color and background color

--- I Am Rich SwiftUI/ContentView.swift
+++ I Am Rich SwiftUI/ContentView.swift
@@ -9,13 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, World!")
+        ZStack {
+            Color(.systemTeal)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                Text("I Am Rich").font(.system(size: 40)).fontWeight(.bold).foregroundColor(Color.white)
+            }
         }
-        .padding()
     }
 }
 
